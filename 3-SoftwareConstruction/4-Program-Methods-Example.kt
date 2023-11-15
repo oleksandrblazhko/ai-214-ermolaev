4520563c08c964812ab969a02f3ef4a592336ba5
Venom2281 <94124037+Venom2281@users.noreply.github.com>
2023-11-15 22:39:17 +0200

    Update 4-Program-Methods-Example.kt

--- 3-SoftwareConstruction/4-Program-Methods-Example.kt
+++ 3-SoftwareConstruction/4-Program-Methods-Example.kt
@@ -10,17 +10,13 @@ class MedicialChat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Инициализация привязок и базы данных Firebase
         binding = ActivityMedicialChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
         databaseReference = FirebaseDatabase.getInstance().reference
 
-        // Получение ссылок на элементы интерфейса
         val bSend = binding.bSend
         val eName = binding.eName
         val eMessage = binding.eMessage
-
-        // Настройка поля ввода для имени
         eName.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_CAP_WORDS
         eName.filters = arrayOf(InputFilter { source, _, _, _, _, _ ->
             // Проверка на пустой ввод или наличие только букв в имени
@@ -32,49 +28,46 @@ class MedicialChat : AppCompatActivity() {
                 ""
             }
         })
-
-        // Настройка ограничения на количество символов в поле ввода сообщения
+    }
+}
+class MedicialChat : AppCompatActivity() {
         val maxLengthMessage = 1000
         val inputFilter = InputFilter.LengthFilter(maxLengthMessage)
         eMessage.filters = arrayOf(inputFilter)
         eMessage.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {}
-
             override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {}
-
             override fun afterTextChanged(editable: Editable?) {
-                // Проверка на достижение максимального числа символов в сообщении
                 val currentLength = editable?.length ?: 0
-                if (currentLength == maxLengthMessage) {
-                    Toast.makeText(applicationContext, "Досягнута максимальна кількість символів.", Toast.LENGTH_SHORT).show()
+                try{
+                    if (currentLength == maxLengthMessage) {
+                        Toast.makeText(applicationContext, "Досягнута максимальна кількість символів.", Toast.LENGTH_SHORT).show()
+                        return -1
+                    }
+                } catch (maxLengthMessage == 0){
+                    Toast.makeText(applicationContext, "Ви не можете відправити запит із пустим текстом.", Toast.LENGTH_SHORT).show()
+                    return -1
                 }
             }
         })
 
-        // Обработчик нажатия на кнопку "Отправить"
         bSend.setOnClickListener {
-            // Получение имени пользователя и сообщения из полей ввода
             val userName = eName.text.toString()
             val userMessage = eMessage.text.toString()
-
-            // Запись сообщения в базу данных Firebase
             writeToDatabase(userName, userMessage)
-
-            // Очистка полей ввода после отправки сообщения
             eName.setText("")
             eMessage.setText("")
         }
-    }
 
-    // Метод для записи сообщения в базу данных Firebase
     private fun writeToDatabase(userName: String, userMessage: String) {
-        // Генерация уникального ключа для сообщения
         val key = databaseReference.child("messages").push().key
         val message = Message(userName, userMessage)
 
-        // Запись сообщения в базу данных, если ключ не пустой
         if (key != null) {
             databaseReference.child("messages").child(key).setValue(message)
         }
     }
 }
+
+
+
